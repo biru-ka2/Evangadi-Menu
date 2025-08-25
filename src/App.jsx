eec37028ba1bfa52f8c4211a0655cf0bce6a0cbd
biru-ka2 <bworku1221@gmail.com>
2025-08-25 23:20:31 +0300

    setup done – future me will thank me

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>setup done – future me will thank me</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
